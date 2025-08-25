18d1f36155a6b51e764b53e617cc82783480d8eb
JeanFabioGruber <jeanfabiogruber2109@gmail.com>
2025-08-25 16:23:14 -0300

    feature: Homepage CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function HomePage() {
   return (
-    <div>
-      <Image 
-        src="/CUTZY.png" 
-        alt="CUTZY Logo" 
-        width={200} 
-        height={100} 
-      />
-      <h1>Bem-vindo a</h1>
-      <p>CUTZY</p>
+    <section className={styles.container}>      
+      <div className={styles.wrapper}>
+        <Image 
+          src="/CUTZY.png" 
+          alt="CUTZY Logo" 
+          width={200} 
+          height={100} 
+        />
+        <h1>Bem-vindo a CUTZY</h1>
+      </div>
+      
       <Link href="/register">
-        <button>Ir para Registro</button>
+        <button className={styles.button}>Ir para Registro</button>
       </Link>
-    </div>
+    </section>
   )
 }
